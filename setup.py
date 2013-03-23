a22a773bd2b253a8eb242ef6565497f5dd4f3237
George Sibble <gsibble@gmail.com>
2013-03-22 19:15:11 -0700

    Updated Library Setup Files

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
     name='subtledata',
-    version='0.0.7',
+    version='0.0.7-2',
     packages=['subtledata', 'subtledata.api', 'subtledata.api.models'],
     url='http://dev.subtledata.com/libraries/python',
     license='MIT',
     author='George Sibble',
     author_email='george.sibble@subtledata.com',
-    description='Integration Library for the Subtledata Platform',
+    description='Integration Library for the SubtleData Platform',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
