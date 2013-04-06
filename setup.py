e4db345e4d3dc36fd3463f63d0e35710f9471388
George Sibble <gsibble@gmail.com>
2013-04-06 13:33:01 -0700

    Bumped Version with Fix for #1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='subtledata',
-    version='0.0.7-5',
+    version='0.0.7-6',
     packages=['subtledata', 'subtledata.api', 'subtledata.api.models'],
     url='http://dev.subtledata.com/libraries/python',
     license='MIT',
