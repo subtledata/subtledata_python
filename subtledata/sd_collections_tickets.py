978451422bd466375886c9621cff5b6c5da2f810
George Sibble <gsibble@gmail.com>
2013-04-06 19:42:52 -0700

    Retrieving Tickets

--- subtledata/sd_collections_tickets.py
+++ subtledata/sd_collections_tickets.py
@@ -5,8 +5,21 @@ from sd_ticket import SDTicket
 
 class SDTicketCollection(SDFirstClassCollection):
 
-    def __init__(self, parent):
+    def __init__(self, parent, tickets):
         super(SDTicketCollection, self).__init__(parent)
 
-    def get(self, ticket_id, user_id=None):
-        return SDTicket(self._api_client, ticket_id=ticket_id, user_id=user_id)
+        self.all = tickets
+
+    def get_with_sd_id(self, ticket_id):
+        for ticket in self.all:
+            if ticket.ticket_id == ticket_id:
+                return ticket_id
+        else:
+            return None
+
+    def get_with_pos_id(self, pos_ticket_id):
+        for ticket in self.all:
+            if ticket.pos_ticket_id == pos_ticket_id:
+                return ticket
+        else:
+            return None
